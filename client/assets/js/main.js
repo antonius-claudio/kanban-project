8c29b0a4265f5a9e8f6aad3129dee53498ee2e24
Claudio Antonius <antonius.claudio@gmail.com>
2020-04-08 16:53:22 +0700

    challange layout with vue

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -1,92 +1,81 @@
 // const url = "https://lit-dawn-18982.herokuapp.com";
-const url = "http://localhost:3000";
-
-function onSignIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    console.log('Name: ' + profile.getName());
-    console.log('Image URL: ' + profile.getImageUrl());
-    console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-    let idToken = googleUser.getAuthResponse().id_token;
-    $.ajax({
-        url: url+'/googleSignIn',
-        method: 'POST',
-        data: {
-            idToken
-        }
-    })
-    .done((result) => {
-        console.log('berhasil');
-        console.log(result);
-    })
-    .fail((err) => {
-        console.log(err);
-    })
-}
-
-$('#btnLogout').click(() => {
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
-})
+// const url = "http://localhost:3000";
   
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!',
-      categories: ['Backlog', 'Todo', 'Done', 'Completed'],
-      tasks: [
+        message: 'Hello Vue!',
+        categories: ['Backlog', 'Todo', 'Done', 'Completed'],
+        tasks: [
             {
                 id:1,
                 title:'ikan',
-                category: this.category[0],
+                category: 'Backlog',
                 user: 'anto1',
                 createdAt: '2020-04-19',
                 isOwner: true
-            },
-            {
+            }, {
                 id:2,
                 title:'ikan2',
-                category: this.category[1],
+                category: 'Todo',
                 user: 'anto2',
                 createdAt: '2020-04-21',
                 isOwner: false
-            },
-            {
+            }, {
                 id:3,
                 title:'ikan3',
-                category: this.category[2],
+                category: 'Done',
                 user: 'anto3',
                 createdAt: '2020-04-22',
                 isOwner: true
-            },
-            {
+            }, {
                 id:4,
                 title:'ikan4',
-                category: this.category[3],
+                category: 'Completed',
                 user: 'anto4',
                 createdAt: '2020-04-23',
                 isOwner: false
             }
-      ]
+        ]
+    },
+    methods: {
+        deleteTask: function (id) {
+            let temp = null;
+            this.tasks.forEach((i, index) => {
+                if(i.id === id) {
+                    temp = index;
+                }
+            });
+            this.tasks.splice(temp, 1);
+        },
+        onSignIn: function (googleUser) {
+            console.log('masuk')
+            var profile = googleUser.getBasicProfile();
+            console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+            console.log('Name: ' + profile.getName());
+            console.log('Image URL: ' + profile.getImageUrl());
+            console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+            let idToken = googleUser.getAuthResponse().id_token;
+            $.ajax({
+                url: url+'/googleSignIn',
+                method: 'POST',
+                data: {
+                    idToken
+                }
+            })
+            .done((result) => {
+                console.log('berhasil');
+                console.log(result);
+            })
+            .fail((err) => {
+                console.log(err);
+            })
+        },
+        btnLogout: function () {
+            var auth2 = gapi.auth2.getAuthInstance();
+            auth2.signOut().then(function () {
+            console.log('User signed out.');
+            });
+        }
     }
-})
-
-$(document).ready(() => {
-    console.log('aap')
-    app;
-    // console.log(url+'/')
-    // $.ajax({
-    //     url: url+'/',
-    //     method: 'GET'
-    // })
-    // .done((result) => {
-    //     $('#testData').append(`
-    //         ${result.message}
-    //     `);
-    // })
-    // .catch((err) => {
-    //     M.toast({html: JSON.stringify(err)});
-    // })
 })
