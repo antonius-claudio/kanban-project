44cbc721d4ac234634f03afa29d3a29458242048
Claudio Antonius <antonius.claudio@gmail.com>
2020-04-10 19:58:55 +0700

    test

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,2 +1,2 @@
-export const url = "http://localhost:3000";
-// export const url = "https://lit-dawn-18982.herokuapp.com";
+export const url = "https://lit-dawn-18982.herokuapp.com";
+// export const url = "http://localhost:3000";
